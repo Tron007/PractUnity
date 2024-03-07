7b7daa5e5d65be60c53e004d5dc8faf2d80a49bc
Tron007 <andrey_ermakov_03@mail.ru>
2024-03-07 14:36:07 +0300

    3 commit

--- My project/Assets/Scripts/MovingObject.cs
+++ My project/Assets/Scripts/MovingObject.cs
@@ -3,27 +3,22 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour
 {
     [SerializeField] private float speed = 100f; // Базовая скорость движения объекта
-    [SerializeField] private float hSpeed = 100f; // Горизонтальная скорость движения объекта
-    [SerializeField] private float runSpeedMultiplier = 2f; // Множитель скорости при беге (Настройте по необходимости)
+    [SerializeField] private float runSpeedMultiplier = 3f; // Множитель скорости при беге (Настройте по необходимости)
     [SerializeField] private float crouchSpeedMultiplier = 0.5f; // Множитель скорости при приседании (Настройте по необходимости)
     [SerializeField] private float jumpForce = 5f; // Сила прыжка объекта
     [SerializeField] private float crouchHeight = 0.5f; // Высота объекта при приседании (Настройте по необходимости)
     private float originalHeight; // Исходная высота объекта
     private Rigidbody _rb; // Компонент Rigidbody объекта
     [SerializeField] private Camera mainCamera; // Основная камера
-    [SerializeField] private Camera firstPersonCamera; // Камера от первого лица
     [SerializeField] private float smoothness = 5f; // Плавность движения камеры
     [SerializeField] private float thirdPersonCameraHeight = 3f; // Высота камеры от третьего лица
-    [SerializeField] private float firstPersonCameraHeight = 1.1f; // Высота камеры от первого лица
     [SerializeField] private float crouchCameraHeight = 0.5f; // Высота камеры при приседании (Настройте по необходимости)
-    [SerializeField] private float cameraSwitchSpeed = 5f; // Скорость переключения камеры
 
     private bool isFirstPersonCameraActive = false; // Флаг активности камеры от первого лица
     private bool isCameraSwitching = false; // Флаг переключения камеры
     private bool isGrounded = true; // Флаг, указывающий, находится ли объект на земле
     private bool isRunning = false; // Флаг, указывающий, что объект бежит
     private bool isCrouching = false; // Флаг, указывающий, что объект приседает
-
 
     private void Awake()
     {
@@ -33,39 +28,31 @@ public class MovingObject : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Определение горизонтальной и вертикальной скорости в зависимости от текущего состояния
         float horizontalSpeed = Input.GetAxis("Horizontal") * GetEffectiveSpeed() * Time.fixedDeltaTime;
         float verticalSpeed = Input.GetAxis("Vertical") * GetEffectiveSpeed() * Time.fixedDeltaTime;
-
-        // Применение скорости к Rigidbody
         _rb.velocity = transform.TransformDirection(new Vector3(horizontalSpeed, _rb.velocity.y, verticalSpeed));
 
-        // Обновление положения камеры
         MoveCamera();
 
-        // Переключение камеры при нажатии C
         if (Input.GetKeyDown(KeyCode.C) && !isCameraSwitching)
         {
             SwitchCamera();
         }
 
-        // Проверка на бег
         isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
-        // Проверка на приседание при нажатии левого или правого Ctrl
         if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
         {
             ToggleCrouch();
         }
 
-        // Проверка на прыжок при нажатии пробела
         if (Input.GetButtonDown("Jump"))
         {
             TryJump();
         }
     }
 
-    // Определение эффективной скорости в зависимости от текущего состояния
+    // Получение эффективной скорости в зависимости от состояния приседания и бега
     float GetEffectiveSpeed()
     {
         if (isCrouching)
@@ -82,37 +69,37 @@ public class MovingObject : MonoBehaviour
         }
     }
 
-    // Обновление положения камеры в зависимости от текущего режима
+    // Обработка движения камеры в зависимости от активного режима
     void MoveCamera()
     {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
         if (isFirstPersonCameraActive)
         {
-            // Обработка вращения камеры относительно игрока
-            float mouseX = Input.GetAxis("Mouse X");
-            float mouseY = Input.GetAxis("Mouse Y");
-
+            // Поворот объекта по горизонтали
             transform.Rotate(Vector3.up * mouseX);
 
-            float newRotationX = firstPersonCamera.transform.eulerAngles.x - mouseY;
-            firstPersonCamera.transform.rotation = Quaternion.Euler(newRotationX, transform.eulerAngles.y, 0f);
+            // Поворот камеры от первого лица по вертикали
+            float newRotationX = mainCamera.transform.eulerAngles.x - mouseY;
+            mainCamera.transform.rotation = Quaternion.Euler(newRotationX, transform.eulerAngles.y, 0f);
 
-            // Обновление положения и высоты камеры в режиме первого лица
+            // Позиционирование камеры от первого лица
             Vector3 newPosition = transform.position + transform.up * GetCameraHeight();
-            firstPersonCamera.transform.position = Vector3.Lerp(firstPersonCamera.transform.position, newPosition, Time.deltaTime * smoothness);
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, newPosition, Time.deltaTime * smoothness);
         }
         else
         {
-            if (mainCamera != null)
-            {
-                // Обновление положения камеры в режиме третьего лица
-                Vector3 targetPosition = transform.position - transform.forward * 5f + Vector3.up * GetCameraHeight();
-                mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, targetPosition, Time.deltaTime * smoothness * cameraSwitchSpeed);
-                mainCamera.transform.LookAt(transform.position);
-            }
+            // Позиционирование камеры от третьего лица
+            Vector3 targetPosition = transform.position - transform.forward * 5f + Vector3.up * GetCameraHeight();
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, targetPosition, Time.deltaTime * smoothness);
+
+            // Наведение камеры на объект
+            mainCamera.transform.LookAt(transform.position + transform.forward * 5f);
         }
     }
 
-    // Определение высоты камеры в зависимости от текущего состояния
+    // Получение высоты камеры в зависимости от состояния приседания и режима камеры
     float GetCameraHeight()
     {
         if (isCrouching)
@@ -121,7 +108,7 @@ public class MovingObject : MonoBehaviour
         }
         else if (isFirstPersonCameraActive)
         {
-            return firstPersonCameraHeight;
+            return 1f; // Нулевая высота для камеры от первого лица
         }
         else
         {
@@ -129,16 +116,13 @@ public class MovingObject : MonoBehaviour
         }
     }
 
-    // Переключение между камерами
+    // Переключение между режимами камеры
     void SwitchCamera()
     {
         isFirstPersonCameraActive = !isFirstPersonCameraActive;
 
-        mainCamera.enabled = !isFirstPersonCameraActive;
-        firstPersonCamera.enabled = isFirstPersonCameraActive;
-
+        // Установка флага переключения камеры и задержка сброса флага
         isCameraSwitching = true;
-
         Invoke("ResetCameraSwitchFlag", 1.0f);
     }
 
@@ -148,11 +132,12 @@ public class MovingObject : MonoBehaviour
         isCameraSwitching = false;
     }
 
-    // Включение/выключение приседания
+    // Переключение состояния приседания
     void ToggleCrouch()
     {
         isCrouching = !isCrouching;
 
+        // Изменение размера объекта при приседании или возвращение к исходному размеру
         if (isCrouching)
         {
             _rb.transform.localScale = new Vector3(_rb.transform.localScale.x, crouchHeight, _rb.transform.localScale.z);
@@ -163,7 +148,7 @@ public class MovingObject : MonoBehaviour
         }
     }
 
-    // Попытка выполнить прыжок, если находится на земле
+    // Попытка выполнить прыжок (если находится на земле)
     void TryJump()
     {
         if (isGrounded)
@@ -178,19 +163,20 @@ public class MovingObject : MonoBehaviour
         _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         isGrounded = false;
 
-        // Предотвращаем нежелательные повторные прыжки в воздухе
+        // Задержка перед восстановлением флага "на земле"
         Invoke("ResetGroundedFlag", 0.2f);
     }
 
-    // Сброс флага нахождения на земле
+    // Сброс флага "на земле"
     void ResetGroundedFlag()
     {
         isGrounded = true;
     }
 
-    // Обработка столкновения с землей
+    // Обработка столкновения с объектами
     private void OnCollisionEnter(Collision collision)
     {
+        // Проверка на столкновение с землей (по тегу "Ground")
         if (collision.collider.CompareTag("Ground"))
         {
             isGrounded = true;
